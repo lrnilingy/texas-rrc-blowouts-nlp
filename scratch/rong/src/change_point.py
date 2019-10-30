aa7e6de40634786207089c9ebb49ca4f5a5eb32f
Unknown <rlu@mines.edu>
2019-10-30 16:16:20 -0600

    bar plot showing how the safety records varied

--- scratch/rong/src/change_point.py
+++ scratch/rong/src/change_point.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
 
 raw_data = pd.read_csv('../data/blowouts_new_cleaned.csv')
 
@@ -10,6 +12,19 @@ print(raw_data['2016'])
 print(raw_data['2016'].shape[0])
 
 year_rng = range(1969, 2020, 1)
-print(*year_rng)
+
+i = 0
+count_data = np.empty(len(year_rng))
+for year in year_rng:
+    count_data[i] = raw_data[str(year)].shape[0]
+    i += 1
+
+print(count_data)
+
+plt.bar(year_rng, count_data, color="#348ABD")
+plt.xlabel('Date')
+plt.ylabel('Count of well control incidents')
+plt.title('Did the safety records change over time?')
+plt.show()
 
 pass
